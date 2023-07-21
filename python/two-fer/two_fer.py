b353239dbb84e90d92ebf3207af796f8f52b6abb
Biju Punalor <biju.punalor@gmail.com>
2023-07-21 19:47:20 +0530

    Done - very simple exercise about f-strings

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,3 +1,20 @@
+#
+# Formatted string literals (also called f-strings for short) let you include
+# the value of Python expressions inside a string by prefixing the string
+# with f or F and writing expressions as {expression}
+#
+# https://docs.python.org/3/tutorial/inputoutput.html#formatted-string-literals
+#
+
+""" demonstrate f-strings using an example"""
+
 def two_fer(name="you"):
+    """
+    A bakery that has a holiday offer where you can buy two cookies 
+    for the price of one ("two-fer one!").
+    
+    Determine what you will say as you give away the extra cookie to a friend 
+    """
+
     answer = f"One for {name}, one for me."
     return answer
