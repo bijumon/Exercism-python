dc96f137e730414a7225402b7d6c74f98da1a393
Biju Punalor <biju.punalor@gmail.com>
2025-07-28 20:59:16 +0530

    finished pig-latin (phew! that was hard. revist later)

--- python/pig-latin/pig_latin.py
+++ python/pig-latin/pig_latin.py
@@ -1,44 +1,39 @@
-vowel = ('a','e','i','o','u')
-starts_with = ('xr','yt')
+#!/usr/bin/env python3
 
-def split_word(word: str):
-    """
-    split word into until first vowel
-    return tuple with first consonents and rest of word
-    """
+"""
+note: `pytest -s | --capture=no` shows print() output
 
-    if not word:
-        return None
+start with a vowel, append "ay" at the end. 
+starts with consonants, move to the end one by one until a vowel is found.
+words beginning with "x" or "y" followed by a consonant, no further transformation.
+keep the “u” attached to “q” when moving characters around
+
+my -> ym -> ymay
+square -> quares -> aresqu -> aresquay
+yellow -> ellowy -> ellowyay
+rhythm -> hythmr -> ythmrh -> ythmrhay
+"""
+
+vowels = ('a','e', 'i', 'o', 'u')
+
+
+def pig_latin(word):
+
+    # stages list stores intermediate transforms of the word
+    stages = [word]
     
-    consonents_list = []
-    for char in word:
-        if not char in vowel and char.isalpha():
-            consonents_list.append(char)
-        else:
+    while not word[0] in vowels:
+        if word[0] in 'xy' and not word[1] in vowels:
             break
+        word = word[1:] + word[0]
+        if word[-1] == 'q' and word[0] == 'u':
+            word = word[1:] + 'u'
+        stages.append(word)
     
-    if not consonents_list:
-        return None
+    stages.append(word+"ay")
+    print(" -> ".join(stages))
 
-    consonents = ''.join(consonents_list)
-    rest_of_word = ''.join(word.split(consonents)[1:])
-    return (consonents, rest_of_word)
+    return word + 'ay'
 
-def translate(text: str):
-    result = []
-    for word in text.split():
-        if word.startswith(vowel) or word.startswith(starts_with):
-            result.append(text + "ay")
-        elif "qu" in word:
-            x = word.split("qu")
-            result.append(f"{x[1]}{x[0]}quay")
-        elif "y" in word:
-            y = word.split("y")
-            result.append(f"y{y[1]}{y[0]}ay")
-        else:
-            first, rest = split_word(word)
-            if first and rest:
-                result.append(rest + first + "ay")
-
-    return ' '.join(result)
-
+def translate(sentence):
+    return ' '.join([pig_latin(word) for word in sentence.split()])
