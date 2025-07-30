18eb09aadd33d0f079ef738a90529dec34774788
bijumon <biju.punalor+aluva@gmail.com>
2025-07-30 11:51:33 +0530

    finished matching bracket exercise

--- python/matching-brackets/matching_brackets.py
+++ python/matching-brackets/matching_brackets.py
@@ -1,2 +1,10 @@
-def is_paired(input_string):
-    pass
+def is_paired(text: str) -> bool:
+    pairs = { '}':'{', ']':'[', ')':'(' }
+    brackets = []
+    for chr in text:
+        if chr in '{[(':
+            brackets.append(chr)
+        elif chr in '}])':
+            if not brackets or brackets.pop() != pairs[chr]:
+                return False
+    return not brackets
