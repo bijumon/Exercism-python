362b06d12115045841f81b8be9eb4d63179054df
Biju Punalor <biju.punalor@gmail.com>
2025-07-31 17:24:06 +0530

    finished exercise rotational cipher

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,7 @@
-def rotate(text, key):
-    pass
+
+def rotate(text: str, key: int) -> str:
+    chars = "abcdefghijklmnopqrstuvwxyz"
+
+    new_chars = chars[key:] + chars[:key]
+    trans_table = str.maketrans(chars + chars.upper(), new_chars + new_chars.upper())
+    return text.translate(trans_table)
