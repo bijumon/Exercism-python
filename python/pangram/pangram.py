a3a21e0d75f5f741b5cdda4b48d847b21c123a1c
Biju Punalor <biju.punalor@gmail.com>
2025-08-02 08:55:41 +0530

    updated solution to exervise - pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,7 +2,7 @@
 def is_pangram(sentence: str) -> bool:
     alphabets = list("abcdefghijklmnopqrstuvwxyz")
 
-    for char in sentence.lower():
-        if char in alphabets:
-            alphabets.remove(char)
-    return True if not alphabets else False
+    for char in alphabets:
+        if char not in sentence.lower():
+            return False
+    return True
